2cf91d4a6f4bc445c845133a6b7da38cfbf7222f
Maksim <Mp-104@hotmail.com>
2023-09-28 10:40:52 +0200

    pl

--- src/com/Maksim/demo/Main.java
+++ src/com/Maksim/demo/Main.java
@@ -5,34 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        /*int[] numbers = {0,10,100,1000,};
-        int test;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("crash program");
-
-        scanner.nextLine();*/
-
-        /*for (int i = 0; i < numbers.length + 1; i++) {  //Index 4 out of bounds for length 4
-            System.out.println(numbers[i]);
-
-        }*/
-
-        System.out.println("before crash");
-
-        try {
-            // try to execute code
-            System.out.println("Inside - TRY ");
-            System.out.println(2/0);
-
-        } catch (Exception error) {
-            // catch error if it FAILS
-            System.out.println("Inside - CATCH ");
-            error.printStackTrace();
-        }
-
-
-        System.out.println("after crash");
 
         }
 
