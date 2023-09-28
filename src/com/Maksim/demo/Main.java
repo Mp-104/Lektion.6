1931c428982bf8819f8fc9038fd1723ae1d39e68
Maksim <Mp-104@hotmail.com>
2023-09-28 11:59:39 +0200

    enTillCommit

--- src/com/Maksim/demo/Main.java
+++ src/com/Maksim/demo/Main.java
@@ -5,7 +5,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        int [] scores = {0, 155, 500};
 
+        for (int i = 0; i < scores.length; i++){
+            System.out.println(scores[i]);
+
+        }
+
+
+
+
+        /* TODO
+        *    How should we structure our project?
+        *    How should one "think"?
+        *    Where does one "start"?
+        *    Were does one "end"?
+        * */
+
+        /* TODO - E-Commerce APP
+        *  Order Product
+        *  Pay for product
+        *  Ship product
+        *  End
+        * */
+
+        // #1 orderProduct ()
+        // #2 payForProduct ()
+        // #3 shipProduct ()
+        // #4 sendConfirmationEmail ()
+
+        //
 
         }
 
